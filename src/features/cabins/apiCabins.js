7f23e9cd4d95ff1ffd7db3f85bce4c2123a3325c
RomaP28 <posokhovrom@gmail.com>
2024-11-19 21:48:41 +0200

    init

---
+++ src/features/cabins/apiCabins.js
@@ -0,0 +1,14 @@
+import supabase from "../../services/supabase.js";
+
+export async function getCabins() {
+	const { data, error } = await supabase
+		.from('cabins')
+		.select('*');
+	
+	if (error) {
+		console.error(error)
+		throw new Error("Cabins couldn't be loaded:");
+	}
+	
+	return data;
+}
---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,23 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import {useEffect} from "react";
+import {getCabins} from "../features/cabins/apiCabins.js";
+
+function Cabins() {
+    
+    useEffect(function() {
+        getCabins().then((data) => {
+            console.log(data);
+        })
+    }, []);
+    
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">All cabins</Heading>
+      <p>TEST</p>
+        <img src="https://kymtbusykcvuahoaiayz.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" alt="image"/>
+    </Row>
+  );
+}
+
+export default Cabins;
