5104ea5034cd377d055d40c5715fe48218aaf358
RomaP28 <posokhovrom@gmail.com>
2024-11-25 09:29:54 +0200

    modal custom hook

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {HiXMark} from "react-icons/hi2";
 import {createPortal} from "react-dom";
 import {cloneElement, createContext, useContext, useState} from "react";
+import {useOutsideClick} from "../hooks/useOutsideClick.js";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -73,11 +74,13 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
 	const { openName, close } = useContext(ModalContext);
+	const ref = useOutsideClick(close);
+	
 	if (name !== openName) return null;
 	
 	return createPortal(
 		<Overlay>
-			<StyledModal>
+			<StyledModal ref={ref}>
 				<Button onClick={close}>
 					<HiXMark />
 				</Button>
