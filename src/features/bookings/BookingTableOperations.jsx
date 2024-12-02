384834f40ca168b6f1dac5011959653af22b31a9
RomaP28 <posokhovrom@gmail.com>
2024-12-02 19:12:16 +0200

    api sorting

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -19,10 +19,7 @@ function BookingTableOperations() {
         options={[
           { value: "startDate-desc", label: "Sort by date (recent first)" },
           { value: "startDate-asc", label: "Sort by date (earlier first)" },
-          {
-            value: "totalPrice-desc",
-            label: "Sort by amount (high first)",
-          },
+          { value: "totalPrice-desc", label: "Sort by amount (high first)" },
           { value: "totalPrice-asc", label: "Sort by amount (low first)" },
         ]}
       />
--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings.js";
+import {useSearchParams} from "react-router-dom";
 
 export function useBookings() {
+	const [searchParams] = useSearchParams();
+	
+	const filterValue = searchParams.get('status');
+	const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
+	
+	
 	const {isLoading, error, data: bookings} = useQuery({
-		queryKey: ['bookings'],
-		queryFn: getBookings,
+		queryKey: ['bookings', filter],
+		queryFn: () => getBookings({ filter }),
 	})
 	console.log(bookings)
 	return { isLoading, error, bookings };
