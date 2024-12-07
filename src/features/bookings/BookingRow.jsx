59dc37398997c15546c366b3df2d88df7ba30a3f
RomaP28 <posokhovrom@gmail.com>
2024-12-07 23:04:15 +0200

    bookings detail page

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -6,6 +6,9 @@ import Table from "../../ui/Table";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
+import {HiEye} from "react-icons/hi2";
+import Menus from "../../ui/Menus.jsx";
+import {useNavigate} from "react-router-dom";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -48,6 +51,8 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate();
+  
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -81,6 +86,15 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+        
+        <Menus.Menu>
+            <Menus.Toggle id={bookingId} />
+            <Menus.List id={bookingId}>
+                <Menus.Button icon={<HiEye />} onClick={() => navigate(`/booking/${bookingId}`)}>
+                    See details
+                </Menus.Button>
+            </Menus.List>
+        </Menus.Menu>
     </Table.Row>
   );
 }
