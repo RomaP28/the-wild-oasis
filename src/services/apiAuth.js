d9bd7da322fd388e7dc21aa3f56ec40eec5366a5
RomaP28 <posokhovrom@gmail.com>
2024-12-22 11:16:55 +0200

    dark mode

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -47,7 +47,7 @@ export async function updateCurrentUser({password, fullName, avatar}) {
 	const { error: storageError } = await supabase.storage.from('avatars').upload(fileName, avatar);
 	if(storageError) throw new Error(storageError.message)
 	
-	const { data: updatedUser, error: error2} = supabase.auth.updateUser({data: {
+	const { data: updatedUser, error: error2} = await supabase.auth.updateUser({data: {
 		avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
 		}})
 	
