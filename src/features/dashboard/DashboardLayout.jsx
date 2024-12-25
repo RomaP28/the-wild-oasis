a3a4e7b80daaa4f8e3765ff32c200d8f309a31ed
RomaP28 <posokhovrom@gmail.com>
2024-12-25 17:51:40 +0200

    building dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+	return (
+		<StyledDashboardLayout>
+			<div>Statistics</div>
+			<div>Today&lsquo;s</div>
+			<div>Chart stay durations</div>
+			<div>Chart of sales</div>
+		</StyledDashboardLayout>
+	)
+}
+
+export default DashboardLayout;
